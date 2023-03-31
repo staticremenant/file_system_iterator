4b83efe83b29f9738dc113a6246b68cc096ec937
staticremenant <mat.bodrov@gmail.com>
2023-03-31 17:51:47 +0300

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='file_iterator',
+    version='1.0.0',
+    author='R&EC SPb ETU',
+    author_email='info@nicetu.spb.ru',
+    url='http://nicetu.spb.ru',
+    description='Разработка класса-итератора по файловой системе на Python',
+    long_description="",
+    zip_safe=False,
+    packages=['file_iterator'],
+)
